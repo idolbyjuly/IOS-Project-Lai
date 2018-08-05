6ffa9654b18e60eb373ea55d86256f13fb283a1a
JulyJ <idolbyjuly@gmail.com>
2018-08-05 15:08:49 -0700

    the project for ios app development

---
+++ Caldenar Master/Caldenar MasterTests/Caldenar_MasterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Caldenar_MasterTests.swift
+//  Caldenar MasterTests
+//
+//  Created by 姜来 on 2018/8/5.
+//  Copyright © 2018年 姜来. All rights reserved.
+//
+
+import XCTest
+@testable import Caldenar_Master
+
+class Caldenar_MasterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
